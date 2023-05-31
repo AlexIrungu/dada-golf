2d181c9721ef2b158ef2869b276cab8de5c3f94d
AlexIrungu <aleaxmuiruri@gmail.com>
2023-05-31 16:02:41 +0300

    updated gallery

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -1,29 +1,21 @@
 import React from 'react';
-import './Gallery.css'
+import './Gallery.css';
 import { importAllImages } from '../utils/utils';
 
-
-
 function Gallery() {
-  // Use require.context to get all the image files in a directory
-  // const images = require.context('./highlights', true, /\.(png|jpe?g|svg|jpg)$/);
-
-const images = importAllImages(
-  require.context(
-    "./highlights",
-    false,
-    /\.(png|jpe?g|svg)$/
-  )
-);
-
-
+  const images = importAllImages(
+    require.context(
+      "./highlights",
+      false,
+      /\.(png|jpe?g|svg)$/
+    )
+  );
 
   return (
-
     <div className='gallery-wrapper'>
-      {Object.values(images).map((value,index) => (
-        <div className='gallery-item' key={index} >
-        <img src={value} alt='dada gallery' />
+      {Object.values(images).map((value, index) => (
+        <div className='gallery-item' key={index}>
+          <img src={value} alt='dada gallery' />
         </div>
       ))}
     </div>
