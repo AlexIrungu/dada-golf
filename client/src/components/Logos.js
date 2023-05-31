026957d93ec5ccb5878ba89be701af7dc1437817
AlexIrungu <aleaxmuiruri@gmail.com>
2023-05-31 17:04:20 +0300

    animating logos

--- client/src/components/Logos.js
+++ client/src/components/Logos.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Logos.css';
 import download from '../assets/download.jpeg';
 import dada from '../assets/dada.png';
@@ -8,25 +8,39 @@ import brain from '../assets/brain.png';
 import mater from '../assets/mater.png';
 
 function Logos() {
+  const logosRef = useRef(null);
+
+  useEffect(() => {
+    const logos = logosRef.current.getElementsByClassName('logo-item');
+
+    let delay = 0;
+    for (let i = 0; i < logos.length; i++) {
+      const logo = logos[i];
+      logo.style.animationDelay = `${delay}s`;
+      delay += 0.3;
+    }
+  }, []);
+
   return (
     <div className="logos-container">
-      <div className="row">
-        <div className="col-md-2 mb-3 text-center logo">
+      <div className="logos-title">Our Partners</div>
+      <div className="logos-list" ref={logosRef}>
+        <div className="logo-item">
           <img src={download} alt="Logo" />
         </div>
-        <div className="col-md-2 mb-3 text-center logo">
+        <div className="logo-item">
           <img src={dada} alt="Logo" />
         </div>
-        <div className="col-md-4 mb-3 text-center logo">
+        <div className="logo-item">
           <img src={aghakan} alt="Logo" />
         </div>
-        <div className="col-md-1 mb-1 text-center logo">
+        <div className="logo-item">
           <img src={sarit} alt="Logo" />
         </div>
-        <div className="col-md-2 mb-3 text-center logo">
+        <div className="logo-item">
           <img src={brain} alt="Logo" />
         </div>
-        <div className="col-md-1 mb-3 text-center logo">
+        <div className="logo-item">
           <img src={mater} alt="Logo" />
         </div>
       </div>
